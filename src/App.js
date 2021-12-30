4303d4e9c85047f2e74cde7563cb4caaad548235
Shams Arefin Khan <robeeen@gmail.com>
2021-12-30 22:48:53 +0600

    Update.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
-  
+  //how many paragraph we want to generate, initially the value will be 0
+  const [count, setCount] = useState(0);
+  //text array default value will be an empty array
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.addEventListener();
+  }
   
   return (
-  <h2>lorem ipsum project setup</h2>
+  <section className='section-center'>
+    <h3>Get some Lorem Ipsum Text</h3>
+    <form className='lorem-form' onSubmit={handleSubmit}>
+    <label htmlFor='amount'>
+      Paragraph:
+    </label>
+    <input type='number' name='amount' id='amount'></input>
+    </form>
+  </section>
     )
 }
 
